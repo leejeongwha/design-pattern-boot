342739437a118682af7da82ed8828da00bf4a177
이정화 <leejeongwha@nhn.com>
2025-06-26 16:52:50 +0900

    컴포지트 패턴 수정

--- src/test/java/com/example/pattern/book/model/BookCompositeTest.java
+++ src/test/java/com/example/pattern/book/model/BookCompositeTest.java
@@ -4,28 +4,29 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BookCompositeTest {
+    
     @Test
-    void 리스트_출력_테스트() {
-        BookShelf shelf1 = new BookShelf("1");
-        BookShelf shelf11 = new BookShelf("1-1");
-        BookShelf shelf12 = new BookShelf("1-2");
-        Book book1 = new Book("마음의온도");
-        Book book2 = new Book("헤드퍼스트디자인패턴");
-        Book book3 = new Book("이펙티브자바");
-        Book book4 = new Book("나의라임오렌지나무");
-
-        shelf1.add(book1);
-
-        shelf11.add(book2);
-        shelf11.add(book3);
-
-        shelf12.add(book4);
-
-        shelf1.add(shelf11);
-        shelf1.add(shelf12);
-
-        shelf1.printList("");
-
+    void 컴포지트패턴_기본_테스트() {
+        // 1. 개별 책들 (Leaf)
+        Book book1 = new Book("헤드퍼스트 디자인패턴");
+        Book book2 = new Book("이펙티브 자바");
+        Book book3 = new Book("클린 코드");
+        
+        // 2. 책장들 (Composite)
+        BookShelf programmingShelf = new BookShelf("프로그래밍");
+        BookShelf mainShelf = new BookShelf("메인 책장");
+        
+        // 3. 책을 책장에 추가
+        programmingShelf.add(book1);
+        programmingShelf.add(book2);
+        programmingShelf.add(book3);
+        
+        // 4. 책장을 다른 책장에 추가 (중첩 구조)
+        mainShelf.add(programmingShelf);
+        
+        // 5. 전체 구조 출력 (단일 인터페이스로 처리)
+        mainShelf.printList("");
+        
         Assertions.assertTrue(true);
     }
 }
