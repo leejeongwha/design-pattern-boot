0870896b5a77d920557fe813dce58fc733ba499d
이정화 <leejeongwha@nhn.com>
2025-06-26 16:31:10 +0900

    테스트 코드 표준 출력 되도록 수정

--- src/test/java/com/example/pattern/PatternApplicationTests.java
+++ src/test/java/com/example/pattern/PatternApplicationTests.java
@@ -3,6 +3,9 @@ package com.example.pattern;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * ./gradlew clean test --tests SingletonServiceTest --console=plain
+ */
 @SpringBootTest
 class PatternApplicationTests {
 
